6d0237725e5cb1031b71e360c19814dea3c46e97
Shenghui Li <shenghui.li@it.uu.se>
2022-05-04 01:40:55 +0200

    update

---
+++ others/example/contour_plot.py
@@ -0,0 +1,46 @@
+import matplotlib.patches as patches
+from matplotlib.path import Path
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patheffects as patheffects
+from matplotlib.lines import Line2D
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.legend_handler import HandlerLine2D
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Patch
+import matplotlib.patches as mpatches
+
+class AnnotationHandler(HandlerLine2D):
+    def __init__(self,ms,*args,**kwargs):
+        self.ms = ms
+        HandlerLine2D.__init__(self,*args,**kwargs)
+    def create_artists(self, legend, orig_handle,
+                       xdescent, ydescent, width, height, fontsize,
+                       trans):
+        xdata, xdata_marker = self.get_xdata(legend, xdescent, ydescent,
+                                             width, height, fontsize)
+        ydata = ((height - ydescent) / 2.) * np.ones(len(xdata), float)
+        legline = FancyArrowPatch(posA=(xdata[0],ydata[0]),
+                                  posB=(xdata[-1],ydata[-1]),
+                                  mutation_scale=self.ms,
+                                  **orig_handle.arrowprops)
+        legline.set_transform(trans)
+        return legline,
+
+
+
+fig, ax = plt.subplots()
+ax.axis([-1,6,0,3])
+ax.plot([1.5,1.5], label="plot")
+# create annotations in the axes
+annotate = ax.annotate('', xy=(0, 1), xytext=(5, 1),
+                       arrowprops={'arrowstyle':'|-|'}, label="endline")
+annotate2 = ax.annotate('', xy=(1, 2), xytext=(3, 2),
+                       arrowprops=dict(arrowstyle='->',color="crimson"), label="arrow")
+# create legend for annotations
+h, l = ax.get_legend_handles_labels()
+ax.legend(handles = h +[annotate,annotate2],
+          handler_map={type(annotate) : AnnotationHandler(5)})
+
+plt.show()
