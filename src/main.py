c3a3ce47c48414e30eb2000342ae487352787a0a
lishenghui <lishenghui.uu@gmail.com>
2022-05-24 22:29:16 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import ray
 import torch
 import inspect
 import importlib
@@ -27,6 +26,7 @@ agg_scheme = getattr(agg_path, options.agg.capitalize())
 
 
 def main(args):
+    
     initialize_logger(options.log_dir)
     device = torch.device("cuda" if args.use_cuda else "cpu")
     
@@ -83,24 +83,6 @@ def main(args):
         use_cuda=args.use_cuda,
         debug=False,
     )
-    # users, _, train_data, test_data = read_data(data_path=options.data_path)
-    # client_builder = ClientBuilder(options=options)
-    # data_manager = DataManager(data_path=options.data_path, batch_size=options.batch_size)
-    # for worker_rank in range(options.num_clients):
-    # for worker_rank, u_id in enumerate(data_manager.clients):
-    #     worker = client_builder.initialize_client(
-    #         # train_data=train_data[u_id], test_data=test_data[u_id],
-    #         trainer=trainer,
-    #         worker_rank=worker_rank,
-    #         model=model,
-    #         optimizer=optimizer,
-    #         loss_func=loss_func,
-    #         device=device,
-    #         use_actor=args.use_actor,
-    #         is_fedavg=args.fedavg,
-    #         kwargs={},
-    #     )
-    #     trainer.add_client(worker)
     
     trainer.setup_clients(options.data_path, model, loss_func, device, optimizer)
     if args.use_actor:
@@ -110,8 +92,7 @@ def main(args):
     
     for epoch in range(1, options.round + 1):
         if args.fedavg:
-            trainer.train_fedavg_v1(epoch, options.local_round)
-            # trainer.train_fedavg(epoch)
+            trainer.train_fedavg(epoch, options.local_round)
         else:
             trainer.train(epoch)
         evaluator.evaluate(epoch)
@@ -120,6 +101,7 @@ def main(args):
 
 
 if __name__ == "__main__":
+    import ray
     if not ray.is_initialized():
         # ray.init(local_mode=True, include_dashboard=True, num_gpus=options.num_gpus)
         ray.init(include_dashboard=True, num_gpus=options.num_gpus)
