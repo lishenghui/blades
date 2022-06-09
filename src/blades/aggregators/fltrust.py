b9457085cb2827316cf4fbede66cd956046a69b7
Shenghui Li <bladesteam.uu@gmail.com>
2022-06-09 19:48:36 +0200

    clean code

--- src/blades/aggregators/fltrust.py
+++ src/blades/aggregators/fltrust.py
@@ -1,7 +1,9 @@
+from typing import List
+
 import torch
+
 from blades.client import BladesClient
-from blades.server import BladesServer
-from typing import Any, Callable, Optional, Union, List
+
 
 class Fltrust(object):
     
@@ -10,30 +12,22 @@ class Fltrust(object):
         assert len(trusted_clients) == 1
         trusted_client = trusted_clients[0]
         
-        untrusted_clients =[client for client in clients if not client.get_is_trusted()]
+        untrusted_clients = [client for client in clients if not client.get_is_trusted()]
         trusted_update = trusted_client.get_update()
         trusted_norm = torch.norm(trusted_update).item()
         untrusted_updates = list(map(lambda w: w.get_update(), untrusted_clients))
         cosine_similarity = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
         ts = torch.Tensor(list(map(lambda update:
-                     torch.nn.functional.relu(
-                         cosine_similarity(trusted_update, update)
-                     ),
-                    untrusted_updates,
-                    ))
-             )
-        pseudo_gradients = torch.vstack(list(map(lambda update:
-                                    update * trusted_norm / torch.norm(update).item()
-                     ,
-                    untrusted_updates,
-                    ))
-             )
+                                   torch.nn.functional.relu(
+                                       cosine_similarity(trusted_update, update)
+                                   ),
+                                   untrusted_updates,
+                                   ))
+                          )
+        pseudo_gradients = torch.vstack(list(map(lambda update: update * trusted_norm / torch.norm(update).item(),
+                                                 untrusted_updates,
+                                                 )
+                                             )
+                                        )
         true_update = (pseudo_gradients.T @ ts) / ts.sum()
         return true_update
-
-
-class FltrustServer(BladesServer):
-    def __int__(self, *args, **kwargs):
-        super(FltrustServer, self).__int__(*args, **kwargs)
-    
-    
