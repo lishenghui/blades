Fixed a memory leakage problem

src/blades/client.py
@@ -1,7 +1,7 @@
 import copy
 import logging
 from collections import defaultdict
-from typing import Union, Tuple, Optional
+from typing import Union, Optional
 
 import ray.train as train
 import torch
@@ -23,6 +23,7 @@ class BladesClient(object):
     _is_trusted: bool = False
     device: str = 'cpu'
     _state = defaultdict(dict)
+    
     def __init__(
             self,
             id: Optional[str] = None,
@@ -66,7 +67,7 @@ class BladesClient(object):
     def is_trusted(self):
         return self._is_trusted
     
-    def trust(self, trusted: Optional[bool]=True) -> None:
+    def trust(self, trusted: Optional[bool] = True) -> None:
         r"""
         Trusts the client as an honest participant. This property is useful
         for trust-based algorithms.
@@ -107,7 +108,7 @@ class BladesClient(object):
         """
         self.model = self.model.to(self.device)
         self.model.train()
-
+    
     def on_train_batch_begin(self, data, target, logs=None):
         """Called at the beginning of a training batch in `local_training` methods.
 
@@ -118,7 +119,7 @@ class BladesClient(object):
          :param logs: Dict. Aggregated metric results up until this batch.
          """
         return data, target
-        
+    
     def evaluate(self, round_number, test_set, batch_size, metrics, use_actor=True):
         dataloader = DataLoader(dataset=test_set, batch_size=batch_size)
         self.model.eval()
@@ -191,30 +192,44 @@ class BladesClient(object):
         :param update: a vector of local update
         """
         self._state['saved_update'] = update.detach()
-
+    
     def _get_saved_update(self) -> torch.Tensor:
         return self._state['saved_update']
-
+    
     def _save_para(self) -> None:
-        for group in self.optimizer.param_groups:
-            for p in group["params"]:
-                if not p.requires_grad:
-                    continue
-                param_state = self._state[p]
-                param_state["saved_para"] = torch.clone(p.data).detach()
+        for name, param in self.model.named_parameters():
+            if not param.requires_grad:
+                continue
+            self._state['saved_para'][name] = torch.clone(param.data).detach()
+        
+        # for group in self.optimizer.param_groups:
+        #     for p in group["params"]:
+        #         if not p.requires_grad:
+        #             continue
+        #         param_state = self._state[p]
+        #         param_state["saved_para"] = torch.clone(p.data).detach()
     
     def _get_para(self, current=True) -> None:
         layer_parameters = []
         
-        for group in self.optimizer.param_groups:
-            for p in group["params"]:
-                if p.grad is None:
-                    continue
-                if current:
-                    layer_parameters.append(p.data.view(-1))
-                else:
-                    param_state = self._state[p]
-                    layer_parameters.append(param_state["saved_para"].data.view(-1))
+        for name, param in self.model.named_parameters():
+            if not param.requires_grad:
+                continue
+            if current:
+                layer_parameters.append(param.data.view(-1))
+            else:
+                saved_param = self._state['saved_para'][name]
+                layer_parameters.append(saved_param.data.view(-1))
+        
+        # for group in self.optimizer.param_groups:
+        #     for p in group["params"]:
+        #         if p.grad is None:
+        #             continue
+        #         if current:
+        #             layer_parameters.append(p.data.view(-1))
+        #         else:
+        #             param_state = self._state[p]
+        #             layer_parameters.append(param_state["saved_para"].data.view(-1))
         return torch.cat(layer_parameters).to('cpu')
 
 

src/blades/simulator.py
@@ -1,8 +1,9 @@
+import importlib
 import logging
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from time import time
 from typing import Any, Callable, Optional, Union, List
-import importlib
+
 import numpy as np
 import ray
 import torch
@@ -137,7 +138,7 @@ class Simulator(object):
             self.aggregator = agg_scheme(**agg_param)
         else:
             self.aggregator = aggregator
-        
+    
     def _setup_clients(self, attack: str, num_byzantine, attack_param):
         import importlib
         if attack is None:
@@ -168,7 +169,6 @@ class Simulator(object):
         """
         for id in ids:
             self._clients[id].trust()
-            
     
     def cache_random_state(self) -> None:
         # This function should be used for reproducibility
@@ -219,11 +219,11 @@ class Simulator(object):
         client_groups = np.array_split(list(self._clients.values()), len(self.ray_actors))
         all_results = self.actor_pool.map(
             lambda actor, clients:
-                actor.local_training.remote(
-                    clients=clients,
-                    model=global_model,
-                    local_round=num_rounds,
-                ),
+            actor.local_training.remote(
+                clients=clients,
+                model=global_model,
+                local_round=num_rounds,
+            ),
             client_groups
         )
         
@@ -371,8 +371,10 @@ class Simulator(object):
             if lr_scheduler:
                 lr_scheduler.step()
                 client_lr = lr_scheduler.get_last_lr()[0]
-            else:
-                client_lr = self.server_opt.param_groups[0]['lr']
+            # else:
+            
+            # client_lr = self.server_opt.param_groups[0]['lr']
+            # client_lr = self.server_opt.param_groups[0]['lr']
             
             ret.append(time() - round_start)
             print(f"E={global_rounds}; Learning rate = {client_lr:}; Time cost = {time() - global_start}")