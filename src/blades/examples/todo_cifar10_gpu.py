dbcf700af8ca30551d9783cd8f92fa038328ceb5
lishenghui <lishenghui.uu@gmail.com>
2022-08-07 23:46:02 +0800

    fix a minor bug

--- src/blades/examples/todo_cifar10_gpu.py
+++ src/blades/examples/todo_cifar10_gpu.py
@@ -16,17 +16,19 @@ from blades.simulator import Simulator
 from blades.datasets import CIFAR10
 from blades.models.cifar10 import CCTNet
 
-cifar10 = CIFAR10(num_clients=20, iid=True)  # built-in federated cifar10 dataset
+cifar10 = CIFAR10(num_clients=20, train_bs=32, seed=3)  # built-in federated cifar10 dataset
 
 # configuration parameters
 conf_params = {
     "dataset": cifar10,
-    "aggregator": "krum",  # defense: robust aggregation
+    "aggregator": "median",  # defense: robust aggregation
     "num_byzantine": 5,  # number of byzantine input
     "use_cuda": True,
-    "attack": "noise",  # attack strategy
-    # "attack_params":{"n": 20, # attacker parameters
-    #                "m": 5},
+    # "attack": "noise",  # attack strategy
+    "attack": "fangattackmedian", #"mediantailored",  # attack strategy
+    "attack_kws": {
+        # "num_clients": 20,
+        "num_byzantine": 8},
     "num_actors": 20,  # number of training actors
     "gpu_per_actor": 0.19,
     "seed": 1,  # reproducibility
@@ -36,15 +38,16 @@ ray.init(num_gpus=4)
 simulator = Simulator(**conf_params)
 
 model = CCTNet()
-server_opt = torch.optim.Adam(model.parameters(), lr=0.01)
+
 # runtime parameters
 run_params = {
     "model": model,  # global model
-    "server_optimizer": server_opt,  # 'SGD',  # server optimizer
+    "server_optimizer": 'SGD',  # ,server_opt  # server optimizer
     "client_optimizer": 'SGD',  # client optimizer
     "loss": "crossentropy",  # loss function
-    "global_rounds": 400,  # number of global rounds
-    "local_steps": 10,  # number of s"client_lr": 0.1,  # learning rateteps per round
-    
+    "global_rounds": 50,  # number of global rounds
+    "local_steps": 50,  # number of steps per round
+    "server_lr": 1.0,
+    "client_lr": 0.1,  # learning rate
 }
 simulator.run(**run_params)
