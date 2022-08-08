01e7fab177cc61e8a5d05b685fc8173be0d1690c
lishenghui <lishenghui.uu@gmail.com>
2022-08-08 21:28:03 +0800

    update scripts

--- scripts/mnist.sh
+++ scripts/mnist.sh
@@ -2,32 +2,53 @@
 
 # pkill -9 ray
 
-# ray start --head --port=6379
+ray start --head --port=6379
 
-for seed in 0
-do
-    for num_byzantine in 8
+run_all_attacks() {
+    for attack in "ipm" "signflipping" "labelflipping" "alie" "noise"
     do
-        for attack in "ipm" #"signflipping" "labelflipping" "alie" "noise"
-        do  
-            for agg in 'trimmedmean' 'geomed' 'median' #'clippedclustering' #'mean' # 'trimmedmean' 'median' 'geomed' 'clippedclustering' 'clustering' 'centeredclipping' 'mean' 'autogm'
-            do
-                args="--global_round 600 --dataset mnist --num_gpus 3 --use-cuda --batch_size 32 --seed $seed --agg ${agg} --num_byzantine ${num_byzantine} --attack $attack"
-                echo ${args}
-                arg_str="\""
-                for var in ${args}
-                    do
-                        arg_str="${arg_str}, \"${var}\""ss
-                    done
-                # python mnist.py ${args}
-                nohup python mnist.py ${args} &
-            done
+        for num_byzantine in 5 8
+        do
+            args="--global_round 600 --dataset mnist --num_gpus 4 --use-cuda --batch_size 32 --seed 0 --agg $1 --num_byzantine $num_byzantine --attack $attack"
+            echo ${args}
+            arg_str="\""
+            for var in ${args}
+                do
+                    arg_str="${arg_str}, \"${var}\""ss
+                done
+            python mnist.py ${args}
+            # nohup python mnist.py ${args} &
         done
     done
+   return 10
+}
 
-    # wait for all pids
-    for pid in ${pids[*]}; do
-        wait $pid
-    done
-    unset pids
+export -f run_all_attacks 
+
+
+for agg in 'trimmedmean' 'geomed' 'median' 'clippedclustering' #'mean' # 'trimmedmean' 'median' 'geomed' 'clippedclustering' 'clustering' 'centeredclipping' 'mean' 'autogm'
+do
+    nohup bash -c "run_all_attacks $agg" &
+    # run_all_attacks ${args} 
 done
+
+
+# for seed in 0
+# do
+#     for num_byzantine in 8
+#     do
+#         for attack in "ipm" #"signflipping" "labelflipping" "alie" "noise"
+#         do  
+#             for agg in 'trimmedmean' 'geomed' 'median' #'clippedclustering' #'mean' # 'trimmedmean' 'median' 'geomed' 'clippedclustering' 'clustering' 'centeredclipping' 'mean' 'autogm'
+#             do
+                
+#             done
+#         done
+#     done
+
+#     # wait for all pids
+#     for pid in ${pids[*]}; do
+#         wait $pid
+#     done
+#     unset pids
+# done
