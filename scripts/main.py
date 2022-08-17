5e3cd2e65435fdcff48a2687c1ee4417c7fec527
lishenghui <lishenghui.uu@gmail.com>
2022-08-18 03:33:01 +0800

    update

--- scripts/main.py
+++ scripts/main.py
@@ -49,7 +49,7 @@ conf_args = {
 simulator = Simulator(**conf_args)
 
 if options.algorithm == 'fedsgd':
-    opt = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9)
+    opt = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.0)
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
         # opt, milestones=[200, 300, 500], gamma=0.5
         opt, milestones=[2000, 3000, 5000], gamma=0.5
