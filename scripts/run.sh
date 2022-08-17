59d0c188e9b9030b9f7c95cd58a4a292f298c7dc
lishenghui <lishenghui.uu@gmail.com>
2022-08-18 01:38:02 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,16 +2,20 @@
 
 # pkill -9 ray
 
+pushd ../src || exit
+  python setup.py develop
+popd || exit
+
 ray start --head --port=6379
 
 run_all_attacks() {
-    for agg in 'clippedclustering' #'mean' 'geomed' 'autogm' 'krum' 'median' 'clustering' 'centeredclipping' 
+    for agg in 'trimmedmean' 'median' 'clippedclustering' #'median' #'mean' 'geomed' 'autogm' 'krum' 'median' 'clustering' 'centeredclipping' 
     do
-        for attack in  "noise"  #"signflipping" "noise" "ipm" "alie" 
+        for attack in  "noise" #"noise"  #"signflipping" "noise" "ipm" "alie" 
         do
-            for num_byzantine in 0
+            for num_byzantine in 5
             do
-                args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4 $5 --num_gpus 4 --use-cuda --batch_size 32 --seed 0 --agg $agg --num_byzantine $num_byzantine --attack $attack"
+                args="--dataset $1 --algorithm $2 --num_clients 20 --global_round $3 --local_round $4 $5 --num_gpus 4 --use-cuda --batch_size 128 --seed 0 --agg $agg --num_byzantine $num_byzantine --attack $attack"
                 echo ${args}
                 nohup python main.py ${args} &
                 # python main.py ${args}
@@ -24,7 +28,7 @@ run_all_attacks() {
 export -f run_all_attacks 
 
 
-dataset='cifar10'
+dataset='mnist'
 # nohup bash -c "run_all_attacks $dataset fedsgd 6000 1 " &
 nohup bash -c "run_all_attacks $dataset fedsgd 6000 1" &
 # run_all_attacks $dataset fedsgd 6000 1
--- scripts/run2.sh
+++ scripts/run2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # pkill -9 ray
-export CUDA_VISIBLE_DEVICES=1,2,3
+# export CUDA_VISIBLE_DEVICES=1,2,3
 ray start --head --port=6379
 
 run_all_aggs() {
-    for attack in "attackclippedclustering" #"noise" "signflipping" "labelflipping" "alie" 
+    for attack in "noise" "signflipping" "labelflipping" "alie" 
     do
         for num_byzantine in 5 #1 3 5 7 9
         do
-            args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 --num_gpus 4 --use-cuda --batch_size 32 --seed 0 --num_byzantine $num_byzantine --attack $attack"
+            args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 --num_gpus 4 --use-cuda --batch_size 128 --seed 0 --num_byzantine $num_byzantine --attack $attack"
             echo ${args}
             python main.py ${args}
         done
@@ -23,9 +23,9 @@ export -f run_all_aggs
 
 dataset='mnist'
 
-for agg in 'clippedclustering' #'centeredclipping' 'mean' 'trimmedmean' 'krum' 'median' 'clustering' 'geomed' 'autogm'
+for agg in 'clippedclustering' 'centeredclipping' 'mean' 'trimmedmean' 'krum' 'median' 'clustering' 'geomed' 'autogm'
 do 
-    run_all_aggs $dataset fedavg 6000 1 $agg
-    # nohup bash -c "run_all_aggs $dataset fedavg 6000 1 $agg" &
+    nohup bash -c "run_all_aggs $dataset fedavg 6000 1 $agg" &
+    nohup bash -c "run_all_aggs $dataset fedavg 6000 1 $agg" &
 done
 
