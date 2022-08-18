4cb1c763235edcc91d6c738f2bbcb9d506fb5a62
lishenghui <lishenghui.uu@gmail.com>
2022-08-18 20:46:10 +0800

    update

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -9,30 +9,11 @@ pushd ../src || exit
 popd || exit
 
 run_all_aggs() {
-    for attack in "ipm" "noise" "signflipping" "labelflipping" "alie" 
+    for attack in "alie"  #"ipm" "noise" "signflipping" "labelflipping" 
     do
-        for num_byzantine in 5 #1 3 5 7 9
+        for serv_momentum in 0.0 0.5 0.95 #1 3 5 7 9
         do
-            args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --use-cuda --batch_size 64 --seed 0 --num_byzantine $num_byzantine --attack $attack"
-            echo ${args}
-            python main.py ${args}
-        done
-    done
-
-    for attack in "ipm"
-    do
-        for num_byzantine in 5 #1 3 5 7 9
-        do
-            args="--dataset $1 --algorithm $2 --ipmlarge --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --use-cuda --batch_size 64 --seed 0 --num_byzantine $num_byzantine --attack $attack"
-            echo ${args}
-            python main.py ${args}
-        done
-    done
-        for attack in "ipm"
-    do
-        for num_byzantine in 0 #1 3 5 7 9
-        do
-            args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --use-cuda --batch_size 64 --seed 0 --num_byzantine $num_byzantine --attack $attack"
+            args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --use-cuda --batch_size 64 --seed 0 --serv_momentum $serv_momentum --attack $attack"
             echo ${args}
             python main.py ${args}
         done
@@ -48,6 +29,6 @@ dataset='cifar10'
 for agg in 'clippedclustering' 'median' 'trimmedmean' 'centeredclipping' 'mean' 'krum' 'clustering' 'geomed' 'autogm'
 do 
     nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg" &
-    nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg  --noniid" &
+    # nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg  --noniid" &
 done
 
