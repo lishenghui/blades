88fee71a8fc8523ea80ad57f2b3f89929bf93726
lishenghui <lishenghui.uu@gmail.com>
2022-08-22 19:55:40 +0800

    update

--- scripts/run_batch_szie.sh
+++ scripts/run_batch_szie.sh
@@ -10,9 +10,9 @@ popd || exit
 
 run_all_aggs() {
     serv_momentum=0.9
-    for attack in "alie"
+    for attack in "noise"
     do
-        for batch_size in 128 512 2500
+        for batch_size in 2500 512 128 
         do
             args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --num_byzantine 5 --use-cuda --batch_size $batch_size --seed 0 --serv_momentum $serv_momentum --attack $attack"
             echo ${args}
@@ -27,7 +27,7 @@ export -f run_all_aggs
 
 dataset='cifar10'
 
-for agg in 'clippedclustering' 'median' 'trimmedmean' 'centeredclipping' 'mean' 'krum' 'clustering' 'geomed' 'autogm'
+for agg in 'clippedclustering' 'median' 'trimmedmean' 'centeredclipping' 'clustering' 'geomed' 'autogm' 'mean' 'krum'
 do 
     nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg" &
     # nohup bash -c "run_all_aggs $dataset fedavg 600 50 $agg  --noniid" &
