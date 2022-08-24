0d179fc1c4a6e0bb2df7503ce41913aa3a7ba281
lishenghui <lishenghui.uu@gmail.com>
2022-08-25 05:22:49 +0800

    update

--- scripts/run2.sh
+++ scripts/run2.sh
@@ -9,13 +9,13 @@ pushd ../src || exit
 popd || exit
 
 run_all_aggs() {
-    for attack in "alie" #"ipm" "noise" "signflipping" "labelflipping" 
+    for attack in "signflipping" #"ipm" "noise" "signflipping" "labelflipping" 
     do
         for serv_momentum in 0.9  #1 3 5 7 9
         do
             args="--dataset $1 --algorithm $2 --global_round $3 --local_round $4  --agg $5 $6 --num_gpus 4 --num_byzantine 5 --use-cuda --batch_size 64 --seed 0 --serv_momentum $serv_momentum --attack $attack"
             echo ${args}
-            python main.py ${args}
+            nohup python main.py ${args} &
         done
     done
     # for attack in "ipm" #"alie" "noise" "signflipping" "labelflipping" 
@@ -42,11 +42,12 @@ run_all_aggs() {
 export -f run_all_aggs 
 
 
-dataset='mnist'
+dataset='cifar10'
 
-for agg in 'signguard' #'median' 'trimmedmean' 'centeredclipping' 'mean' 'krum' 'clustering' 'geomed' 'autogm'
+for agg in 'clippedclustering' #'median' 'trimmedmean' 'centeredclipping' 'mean' 'krum' 'clustering' 'geomed' 'autogm'
 do 
     nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg" &
+    nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg --noniid" &
     # nohup bash -c "run_all_aggs $dataset fedsgd 6000 1 $agg  --noniid" &
 done
 
