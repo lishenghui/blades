fb65a0b8c2dda5d573ac9fce39492bb69a77244c
lishenghui <lishenghui.uu@gmail.com>
2022-09-11 03:42:44 +0800

    update documentation

--- src/blades/aggregators/dnc.py
+++ src/blades/aggregators/dnc.py
@@ -15,12 +15,8 @@ from blades.utils import torch_utils
 
 class Dnc(_BaseAggregator):
     r"""
-         A robust aggregator from paper `"An Experimental Study of Byzantine-Robust sAggregation Schemes in Federated Learning" <https://www.techrxiv.org/articles/preprint/An_Experimental_Study_of_Byzantine-Robust_Aggregation_Schemes_in_Federated_Learning/19560325>`_
+         A robust aggregator from paper `Manipulating the Byzantine: Optimizing Model Poisoning Attacks and Defenses for Federated Learning <https://par.nsf.gov/servlets/purl/10286354>`_
 
-         it separates the client population into two groups based on the cosine similarities
-
-    Args:
-        tau (float): threshold of clipping norm. If it is not given, updates are clipped according the median of historical norm.
     """
     
     def __init__(self, num_byzantine, *, sub_dim=10000, num_iters=1, filter_frac=1.0) -> None:
