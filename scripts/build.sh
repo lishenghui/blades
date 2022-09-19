8501445e981accc41626af7bc8cb6797ee543656
Shenghui Li <56755658+lishenghui@users.noreply.github.com>
2022-09-19 13:39:26 +0200

    Delete build.sh

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-pushd ../src
-  python3 setup.py sdist bdist_wheel
-
-  pushd dist
-    pip uninstall -y byzantinefl
-    pip install --force-reinstall byzantinefl-0.0.1-py3-none-any.whl
-  popd
-popd
