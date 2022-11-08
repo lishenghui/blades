82319ac2b2771efb64ae4ada48dcddc337612b2e
lishenghui <lishenghui.uu@gmail.com>
2022-11-08 23:39:13 +0800

    skip test GPU

--- src/tests/test_core/test_dist.py
+++ src/tests/test_core/test_dist.py
@@ -112,79 +112,75 @@ def test_actormanager():
                 t.set_postfix(loss=test_results[0], top1=test_results[1])
 
 
-def test_actormanager_cross_GPU():
-    device = "cuda"
-    num_clients = 40
-    # set_random_seed()
-    net = CCTNet10().to(device)
-    opt_cls = torch.optim.SGD
-    opt_kws = {"lr": 1.0, "momentum": 0, "dampening": 0}
-    clients = [BladesClient(id=str(id)) for id in range(num_clients)]
-    dataset = CIFAR10(train_bs=64, num_clients=num_clients, seed=0)
-    agg = Mean()
+# def test_actormanager_cross_GPU():
+#     device = "cuda"
+#     num_clients = 40
+#     # set_random_seed()
+#     net = CCTNet10().to(device)
+#     opt_cls = torch.optim.SGD
+#     opt_kws = {"lr": 1.0, "momentum": 0, "dampening": 0}
+#     clients = [BladesClient(id=str(id)) for id in range(num_clients)]
+#     dataset = CIFAR10(train_bs=64, num_clients=num_clients, seed=0)
+#     agg = Mean()
 
-    server_kws = {
-        "model": net,
-        "opt_cls": opt_cls,
-        "opt_kws": {"lr": 0.1, "momentum": 0.9, "dampening": 0},
-        "aggregator": agg,
-    }
+#     server_kws = {
+#         "model": net,
+#         "opt_cls": opt_cls,
+#         "opt_kws": {"lr": 0.1, "momentum": 0.9, "dampening": 0},
+#         "aggregator": agg,
+#     }
 
-    act_mgrs = []
-    num_gpus = 1
-    client_groups = np.array_split(clients, num_gpus)
-    for i in range(num_gpus):
-        if i == 0:
-            server_cls = BladesServer
-            num_gpus_mgr = 0.2
-        else:
-            server_cls = None
-            num_gpus_mgr = 0.2
-        actor_mgr = ActorManager.options(num_gpus=num_gpus_mgr).remote(
-            dataset,
-            net,
-            opt_cls,
-            opt_kws,
-            rank=i,
-            num_actors=5,
-            num_buffers=len(client_groups[i]),
-            num_selected_clients=len(clients),
-            gpu_per_actor=0.15,
-            world_size=num_gpus,
-            server_cls=server_cls,
-            server_kws=server_kws,
-            device=device,
-        )
-        ray.get(actor_mgr.init.remote())
-        act_mgrs.append(actor_mgr)
+#     act_mgrs = []
+#     num_gpus = 1
+#     client_groups = np.array_split(clients, num_gpus)
+#     for i in range(num_gpus):
+#         if i == 0:
+#             server_cls = BladesServer
+#             num_gpus_mgr = 0.2
+#         else:
+#             server_cls = None
+#             num_gpus_mgr = 0.2
+#         actor_mgr = ActorManager.options(num_gpus=num_gpus_mgr).remote(
+#             dataset,
+#             net,
+#             opt_cls,
+#             opt_kws,
+#             rank=i,
+#             num_actors=5,
+#             num_buffers=len(client_groups[i]),
+#             num_selected_clients=len(clients),
+#             gpu_per_actor=0.15,
+#             world_size=num_gpus,
+#             server_cls=server_cls,
+#             server_kws=server_kws,
+#             device=device,
+#         )
+#         ray.get(actor_mgr.init.remote())
+#         act_mgrs.append(actor_mgr)
 
-    ray.get([mgr.init_dist.remote() for mgr in act_mgrs])
-    global_rounds = 4000
-    validate_interval = 100
-    with trange(0, global_rounds + 1) as t:
-        for global_rounds in t:
-            client_groups = np.array_split(clients, num_gpus)
-            ret_ids = [
-                actor_mgr.train.remote(clients=client_group)
-                for (actor_mgr, client_group) in zip(act_mgrs, client_groups)
-            ]
-            ray.get(ret_ids)
-            if global_rounds % validate_interval == 0 and global_rounds > 0:
-                ray.get([mgr.broadcast.remote() for mgr in act_mgrs])
-                ret_ids = [
-                    actor_mgr.evaluate.remote(
-                        clients=client_group,
-                        round_number=global_rounds,
-                        metrics={"top1": top1_accuracy},
-                    )
-                    for (actor_mgr, client_group) in zip(act_mgrs, client_groups)
-                ]
+#     ray.get([mgr.init_dist.remote() for mgr in act_mgrs])
+#     global_rounds = 4000
+#     validate_interval = 100
+#     with trange(0, global_rounds + 1) as t:
+#         for global_rounds in t:
+#             client_groups = np.array_split(clients, num_gpus)
+#             ret_ids = [
+#                 actor_mgr.train.remote(clients=client_group)
+#                 for (actor_mgr, client_group) in zip(act_mgrs, client_groups)
+#             ]
+#             ray.get(ret_ids)
+#             if global_rounds % validate_interval == 0 and global_rounds > 0:
+#                 ray.get([mgr.broadcast.remote() for mgr in act_mgrs])
+#                 ret_ids = [
+#                     actor_mgr.evaluate.remote(
+#                         clients=client_group,
+#                         round_number=global_rounds,
+#                         metrics={"top1": top1_accuracy},
+#                     )
+#                     for (actor_mgr, client_group) in zip(act_mgrs, client_groups)
+#                 ]
 
-                results = ray.get(ret_ids)
-                results = [item for sublist in results for item in sublist]
-                test_results = log_validate(results)
-                t.set_postfix(loss=test_results[0], top1=test_results[1])
-
-
-test_actormanager_cross_GPU()
-# test_actormanager()
+#                 results = ray.get(ret_ids)
+#                 results = [item for sublist in results for item in sublist]
+#                 test_results = log_validate(results)
+#                 t.set_postfix(loss=test_results[0], top1=test_results[1])
