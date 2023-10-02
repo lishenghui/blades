a7f13c8f14f235ece689e0f049b32090a810ad6f
lishenghui <lishenghui.uu@gmail.com>
2023-10-02 22:10:25 +0800

    [Aggregator] Refactor & Unit test for ClippedClustering

--- fllib/aggregators/clippedclustering.py
+++ fllib/aggregators/clippedclustering.py
@@ -3,7 +3,7 @@ from typing import List
 import torch
 import numpy as np
 from numpy import inf
-from sklearn.cluster import AgglomerativeClustering, KMeans
+from sklearn.cluster import AgglomerativeClustering
 
 from fllib.utils import torch_utils
 
@@ -11,14 +11,11 @@ from .aggregators import Mean, Median
 
 
 class Clippedclustering(object):
-    def __init__(
-        self, agg="mean", signguard=False, max_tau=1e5, linkage="average"
-    ) -> None:
+    def __init__(self, agg="mean", max_tau=1e5, linkage="average") -> None:
         super(Clippedclustering, self).__init__()
 
         assert linkage in ["average", "single"]
         self.tau = max_tau
-        self.signguard = signguard
         self.linkage = linkage
         self.l2norm_his = []
         if agg == "mean":
@@ -29,16 +26,37 @@ class Clippedclustering(object):
             raise NotImplementedError(f"{agg} is not supported yet.")
 
     def __call__(self, inputs: List[torch.Tensor]):
-        updates = torch.stack(inputs, dim=0)
-        l2norms = [torch.norm(update).item() for update in updates]
+        # Clip updates based on L2 norm
+        updates = self._clip_updates(inputs)
+
+        # Compute pairwise cosine similarity
+        dis_max = self._compute_cosine_similarity(updates)
+
+        # Cluster updates using AgglomerativeClustering
+        selected_idxs = self._cluster_updates(dis_max)
+        # Compute final values using selected updates
+        values = self._compute_values(selected_idxs, updates)
+
+        return values
+
+    def _clip_updates(self, inputs: List[torch.Tensor]):
+        l2norms = [torch.norm(update).item() for update in inputs]
         self.l2norm_his.extend(l2norms)
         threshold = np.median(self.l2norm_his)
         threshold = min(threshold, self.tau)
 
+        clipped_updates = []
         for idx, l2 in enumerate(l2norms):
             if l2 > threshold:
-                updates[idx] = torch_utils.clip_tensor_norm_(updates[idx], threshold)
+                clipped_updates.append(
+                    torch_utils.clip_tensor_norm_(inputs[idx], threshold)
+                )
+            else:
+                clipped_updates.append(inputs[idx])
 
+        return torch.stack(clipped_updates, dim=0)
+
+    def _compute_cosine_similarity(self, updates):
         num = len(updates)
         dis_max = np.zeros((num, num))
         for i in range(num):
@@ -50,36 +68,22 @@ class Clippedclustering(object):
         dis_max[dis_max == -inf] = 0
         dis_max[dis_max == inf] = 2
         dis_max[np.isnan(dis_max)] = 2
+        return dis_max
+
+    def _cluster_updates(self, dis_max):
         clustering = AgglomerativeClustering(
             metric="precomputed", linkage=self.linkage, n_clusters=2
         )
         clustering.fit(dis_max)
 
-        flag = 1 if np.sum(clustering.labels_) > num // 2 else 0
-        S1_idxs = list(
-            [idx for idx, label in enumerate(clustering.labels_) if label == flag]
-        )
-        selected_idxs = S1_idxs
+        flag = 1 if np.sum(clustering.labels_) > len(dis_max) // 2 else 0
+        selected_idxs = [
+            idx for idx, label in enumerate(clustering.labels_) if label == flag
+        ]
 
-        if self.signguard:
-            features = []
-            num_para = len(updates[0])
-            for update in updates:
-                feature0 = (update > 0).sum().item() / num_para
-                feature1 = (update < 0).sum().item() / num_para
-                feature2 = (update == 0).sum().item() / num_para
+        return selected_idxs
 
-                features.append([feature0, feature1, feature2])
-
-            kmeans = KMeans(n_clusters=2, random_state=0).fit(features)
-
-            flag = 1 if np.sum(kmeans.labels_) > num // 2 else 0
-            S2_idxs = list(
-                [idx for idx, label in enumerate(kmeans.labels_) if label == flag]
-            )
-
-            selected_idxs = list(set(S1_idxs) & set(S2_idxs))
-
+    def _compute_values(self, selected_idxs, updates):
         benign_updates = []
         for idx in selected_idxs:
             benign_updates.append(updates[idx])
---
+++ fllib/aggregators/tests/test_clippedclustering.py
@@ -0,0 +1,24 @@
+import torch
+import unittest
+from fllib.aggregators import Clippedclustering
+
+
+class TestAggregators(unittest.TestCase):
+    def setUp(self):
+        raw_input = torch.Tensor(
+            [
+                [1, 2, 3],
+                [-1, 4, -1],
+                [2.0, 2, 3],
+                [3.0, 1.0, 3],
+            ]
+        )
+        self.input_data = [
+            torch.squeeze(tensor) for tensor in torch.split(raw_input, 1)
+        ]
+        self.clippedclustering = Clippedclustering()
+
+    def test_clippedclustering(self):
+        expected_output = torch.Tensor([1.9596, 1.6532, 2.9596])
+        output = self.clippedclustering(self.input_data)
+        self.assertTrue(torch.allclose(output, expected_output))
